eda6376acb0c9827274db94bfa30aa3786c1197f
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2018-09-11 01:27:07 -0700

    Adds initial logic for clicking the buttons and styling for indicating buttons are clickable

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(() => {
+    // button effects for numbers
     $('.num-area').on('mouseover', event => {
         if (!($(event.currentTarget).hasClass('empty'))) {
             $(event.currentTarget).addClass('num-area-active');
@@ -7,11 +8,21 @@ $(document).ready(() => {
         $(event.currentTarget).removeClass('num-area-active');
     });
 
+    // button effects for operators
     $('.operator-area').on('mouseover', event => {
         if (!($(event.currentTarget).hasClass('empty'))) {
             $(event.currentTarget).addClass('operator-area-active');
         }
     }).on('mouseleave', event => {
         $(event.currentTarget).removeClass('operator-area-active');
+    });
+
+    // button logic
+    $('.button').on('click', event => {
+        if (!($(event.currentTarget).hasClass('empty'))) {
+            // temp code to ensure buttons are working
+            let text = $(event.currentTarget).text();
+            $('.output').text(text);
+        }
     });
 });
