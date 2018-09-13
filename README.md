972faef9e12235eb4ae3627bb9da449dd125383c
Kyle Domaszewicz <kyledomaszewicz@gmail.com>
2018-09-12 17:22:23 -0700

    Refactors code to fit stricter style guidelines

--- README.md
+++ README.md
@@ -16,19 +16,19 @@ None.
 
 #### Optional
 
-This project makes use of ESLint for linting.
+This project makes use of ESLint for linting and Prettier for formatting.
 
 ### Deployment
 
-Move [index.html](index.html), [main.css](main.css), and [main.js](main.js) to the desired location on your web server. The project will be served at 'domain.tlr/path/to/project/index.html'.
+Move [index.html](index.html), [main.css](main.css), and [main.js](main.js) to the desired location on your web server. The project will be served at 'domain.tld/path/to/project/index.html'.
 
 ## Built With
 
-* [JQuery](https://jquery.com/) - The JavaScript support library used
+-   [JQuery](https://jquery.com/) - The JavaScript support library used
 
 ## Authors
 
-* **Kyle Domaszewicz** - [kdomasze](https://github.com/kdomasze)
+-   **Kyle Domaszewicz** - [kdomasze](https://github.com/kdomasze)
 
 ## License
 
@@ -36,4 +36,4 @@ This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md
 
 ## Acknowledgments
 
-* Calculator styling based off of the default material design calculator on Android.
+-   Calculator styling based off of the default material design calculator on Android.
--- main.js
+++ main.js
@@ -1,97 +1,10 @@
-$(document).ready(() => {
-    const entry = {
-        text: ''
-    };
-
-    const $output = $('.output');
-
-    // blinking cursor for output
-    setInterval(() => {
-        $('.output').toggleClass('blinking-cursor');
-    }, 500);
-
-    // button effects for numbers
-    $('.num-area').hover(event => {
-        if (!($(event.currentTarget).hasClass('empty'))) {
-            $(event.currentTarget).toggleClass('num-area-active');
-        }
-    });
-
-    // button effects for operators
-    $('.operator-area').hover(event => {
-        if (!($(event.currentTarget).hasClass('empty'))) {
-            $(event.currentTarget).toggleClass('operator-area-active');
-        }
-    });
-
-    // button logic
-    $('.button').on('click', event => {
-        if (!($(event.currentTarget).hasClass('empty'))) {
-            let buttonText = $(event.currentTarget).text();
-
-            if (buttonText === '÷' || buttonText === '×' || buttonText === '-' || buttonText === '+') {
-                addOperator(entry, buttonText, $output);
-            } else if (buttonText === '.') {
-                addDecimal(entry, $output);
-            } else if (buttonText === 'DEL') {
-                deleteEntry(entry, $output);
-            } else if (buttonText === '=') {
-                calculateResult(entry, $output);
-            } else {
-                addNumber(entry, buttonText, $output);
-            }
-        }
-    });
-});
-
-const deleteEntry = (entry, output) => {
-    if (entry.text.length === 0) {
-        return;
-    }
-
-    // delete character
-    entry.text = entry.text.substring(0, entry.text.length - 1);
-
-    // deleting results in empty output case
-    if (entry.text.length === 0) {
-        output.text('0');
-        return;
-    }
-
-    // display output
-    output.text(entry.text);
-};
-
-const addOperator = (entry, buttonText, output) => {
-    entry.text += buttonText;
-    output.text(entry.text);
-};
-
-const addDecimal = (entry, output) => {
-    let tempSplit = entry.split(/[+-÷×]/);
-    if (!tempSplit[tempSplit.length - 1].includes('.')) {
-        entry.text += '.';
-    }
-    output.text(entry.text);
-};
-
-const addNumber = (entry, buttonText, output) => {
-    entry.text += buttonText;
-    output.text(entry.text);
-};
-
-const calculateResult = (entry, output) => {
-    let result = _parseExpression(entry.text);
-
-    entry.text = '' + result;
-    output.text(entry.text);
-};
+const isNumeric = (num) => !Number.isNaN(num) && !Number.isNaN(parseFloat(num));
 
 const _seperateOutHighOrderOperations = (expression) => {
-    let calculation = [];
+    const calculation = [];
     let temp = '';
 
-    for (let i in expression) {
+    for (let i = 0; i < expression.length; i++) {
         if (expression[i] === '+' || expression[i] === '-') {
             calculation.push(temp);
             temp = '';
@@ -108,12 +21,33 @@ const _seperateOutHighOrderOperations = (expression) => {
     return calculation;
 };
 
+const _handleOperator = (operator, num, result) => {
+    let newResult = result;
+
+    switch (operator) {
+        case '-':
+            newResult = result - num;
+            break;
+        case '×':
+            newResult = result * num;
+            break;
+        case '÷':
+            newResult = result / num;
+            break;
+        case '+':
+        default:
+            newResult = result + num;
+    }
+
+    return newResult;
+};
+
 // this function will calculate all the multiplication and division expression and return
 // an array of numbers and operators (bound to '+' and '-')
 const _calculateLowerOrderExpression = (higherOrderExpression) => {
-    let lowerOrderExpression = [];
+    const lowerOrderExpression = [];
 
-    for (let i in higherOrderExpression) {
+    for (let i = 0; i < higherOrderExpression.length; i++) {
         // we just want to store and move on if the operation isn't multiplication
         // or division
         if (higherOrderExpression[i].includes('+') || higherOrderExpression[i].includes('-')) {
@@ -128,14 +62,14 @@ const _calculateLowerOrderExpression = (higherOrderExpression) => {
                 operator = '÷';
             }
 
-            let splitOperation = higherOrderExpression[i].split(operator);
+            const splitOperation = higherOrderExpression[i].split(operator);
             let tempResult = splitOperation[0];
             // loop through each number in the current operation and ensure they are all
             // multiplied or divided together
             for (let j = 1; j < splitOperation.length; j++) {
                 tempResult = _handleOperator(operator, splitOperation[j], tempResult);
             }
-            lowerOrderExpression.push('' + tempResult);
+            lowerOrderExpression.push(`${tempResult}`);
         }
     }
 
@@ -145,8 +79,7 @@ const _calculateLowerOrderExpression = (higherOrderExpression) => {
 const _calculateResult = (lowerOrderExpression) => {
     let result = 0;
     let lastOperator = '+';
-
-    for (let i in lowerOrderExpression) {
+    for (let i = 0; i < lowerOrderExpression.length; i++) {
         if (isNumeric(lowerOrderExpression[i])) {
             result = _handleOperator(lastOperator, Number(lowerOrderExpression[i]), result);
         } else {
@@ -158,32 +91,120 @@ const _calculateResult = (lowerOrderExpression) => {
 };
 
 const _parseExpression = (joinedExpression) => {
-    let higherOrderExpression = _seperateOutHighOrderOperations(joinedExpression);
-    let lowerOrderExpression = _calculateLowerOrderExpression(higherOrderExpression);
-    let result = _calculateResult(lowerOrderExpression);
+    const higherOrderExpression = _seperateOutHighOrderOperations(joinedExpression);
+    const lowerOrderExpression = _calculateLowerOrderExpression(higherOrderExpression);
+    const result = _calculateResult(lowerOrderExpression);
     return result;
 };
 
-const _handleOperator = (operator, num, result) => {
-    switch (operator) {
-        case '+':
-            result += num;
-            break;
-        case '-':
-            result -= num;
-            break;
-        case '×':
-            result *= num;
-            break;
-        case '÷':
-            result /= num;
-            break;
+const deleteEntry = (entry, output) => {
+    let updatedEntry = entry;
+
+    if (updatedEntry.length === 0) {
+        return updatedEntry;
     }
 
-    return result;
+    // delete character
+    updatedEntry = updatedEntry.substring(0, updatedEntry.length - 1);
+
+    // deleting results in empty output case
+    if (updatedEntry.length === 0) {
+        output.text('0');
+        return updatedEntry;
+    }
+
+    // display output
+    output.text(updatedEntry);
+
+    return updatedEntry;
 };
 
-const isNumeric = (num) => {
-    return !isNaN(num);
+const addOperator = (entry, buttonText, output) => {
+    let updatedEntry = entry;
+
+    updatedEntry += buttonText;
+    output.text(updatedEntry);
+
+    return updatedEntry;
 };
 
+const addDecimal = (entry, output) => {
+    let updatedEntry = entry;
+
+    const tempSplit = entry.split(/[+-÷×]/);
+    if (!tempSplit[tempSplit.length - 1].includes('.')) {
+        updatedEntry += '.';
+    }
+    output.text(updatedEntry);
+
+    return updatedEntry;
+};
+
+const addNumber = (entry, buttonText, output) => {
+    let updatedEntry = entry;
+
+    updatedEntry += buttonText;
+    output.text(updatedEntry);
+
+    return updatedEntry;
+};
+
+const calculateResult = (entry, output) => {
+    let updatedEntry = entry;
+
+    const result = _parseExpression(updatedEntry);
+
+    updatedEntry = `${result}`;
+    output.text(updatedEntry);
+
+    return updatedEntry;
+};
+
+$(document).ready(() => {
+    let entry = '';
+
+    const $output = $('.output');
+
+    // blinking cursor for output
+    setInterval(() => {
+        $('.output').toggleClass('blinking-cursor');
+    }, 500);
+
+    // button effects for numbers
+    $('.num-area').hover((event) => {
+        if (!$(event.currentTarget).hasClass('empty')) {
+            $(event.currentTarget).toggleClass('num-area-active');
+        }
+    });
+
+    // button effects for operators
+    $('.operator-area').hover((event) => {
+        if (!$(event.currentTarget).hasClass('empty')) {
+            $(event.currentTarget).toggleClass('operator-area-active');
+        }
+    });
+
+    // button logic
+    $('.button').on('click', (event) => {
+        if (!$(event.currentTarget).hasClass('empty')) {
+            const buttonText = $(event.currentTarget).text();
+
+            if (
+                buttonText === '÷' ||
+                buttonText === '×' ||
+                buttonText === '-' ||
+                buttonText === '+'
+            ) {
+                entry = addOperator(entry, buttonText, $output);
+            } else if (buttonText === '.') {
+                entry = addDecimal(entry, $output);
+            } else if (buttonText === 'DEL') {
+                entry = deleteEntry(entry, $output);
+            } else if (buttonText === '=') {
+                entry = calculateResult(entry, $output);
+            } else {
+                entry = addNumber(entry, buttonText, $output);
+            }
+        }
+    });
+});
